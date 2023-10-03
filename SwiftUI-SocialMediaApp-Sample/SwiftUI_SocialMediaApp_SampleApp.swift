3571ee65ec233ad9cb5d9cadbe520590c5d7a181
ekayaint <ekayaint@gmail.com>
2023-10-03 12:21:31 +0300

    Initial Commit

---
+++ SwiftUI-SocialMediaApp-Sample/SwiftUI_SocialMediaApp_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SocialMediaApp_SampleApp.swift
+//  SwiftUI-SocialMediaApp-Sample
+//
+//  Created by ekayaint on 3.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SocialMediaApp_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
