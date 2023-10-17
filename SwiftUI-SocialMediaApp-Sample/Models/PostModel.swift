c27a3b8cf01ed4abfd0b753f633a1e6774cd6cae
ekayaint <ekayaint@gmail.com>
2023-10-17 21:02:00 +0300

    Kingfisher added

--- SwiftUI-SocialMediaApp-Sample/Models/PostModel.swift
+++ SwiftUI-SocialMediaApp-Sample/Models/PostModel.swift
@@ -12,4 +12,16 @@ struct Post: Decodable, Identifiable {
     var id: String
     var postTitle: String
     var timestamp: Date
+    var name: String
+    var userUID: String
+    var imageURL: String
+    
+    init(data: [String: Any]) {
+        self.id = data["id"] as? String ?? ""
+        self.postTitle = data["postTitle"] as? String ?? ""
+        self.timestamp = (data["timestamp"] as? Timestamp)?.dateValue() ?? Date()
+        self.name = data["name"] as? String ?? ""
+        self.userUID = data["userUID"] as? String ?? ""
+        self.imageURL = data["imageURL"] as? String ?? ""
+    }
 }
--- SwiftUI-SocialMediaApp-Sample/Views/PostsView.swift
+++ SwiftUI-SocialMediaApp-Sample/Views/PostsView.swift
@@ -6,6 +6,76 @@
 //
 
 import SwiftUI
+import Kingfisher
+
+struct PostComponent: View {
+    let post: Post
+    var postsViewModel = PostsViewModel()
+    @State private var profileImage: UIImage?
+    @State private var isLoadingImage = false
+    
+    var body: some View {
+        VStack {
+            Divider()
+                .padding(.horizontal)
+            
+            HStack {
+                if isLoadingImage {
+                    Image(systemName: "person")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .clipped()
+                        .cornerRadius(50)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 44)
+                                .stroke(Color(.label), lineWidth: 1)
+                        }
+                        .padding(.leading)
+                } else if let image = profileImage {
+                    Image(uiImage: image)
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .clipped()
+                        .cornerRadius(50)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 44)
+                                .stroke(Color(.label), lineWidth: 1)
+                        }
+                        .padding(.leading)
+                    
+                } //: if
+                
+                Text(post.name)
+                    .padding(.vertical, 2)
+                    .padding(.horizontal)
+                    .foregroundStyle(.blue)
+                
+                Spacer()
+                
+            } //: HStack
+            
+            HStack{
+                Text(post.postTitle)
+                    .bold()
+                Spacer()
+                Text(post.timestamp.formatted())
+                    .font(.caption2)
+            } //: HStack
+            .padding()
+            
+            if let url = URL(string: post.imageURL) {
+                KFImage(url)
+            }
+        } //: VStack
+        .onAppear(perform: {
+            isLoadingImage = true
+            postsViewModel.getUserProfileImage(userUID: post.userUID) { image in
+                profileImage = image
+                isLoadingImage = false
+            }
+        })
+    }
+}
 
 struct PostsView: View {
     var body: some View {
